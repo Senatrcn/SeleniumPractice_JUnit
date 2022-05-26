08d2c135ecefd23ac3c545d3cc20a7ef3850dd20
Sena TERCAN <avsenatercan@gmail.com>
2022-05-26 12:41:10 +0300

    sena commit3

--- src/test/java/Alerts/Q2.java
+++ src/test/java/Alerts/Q2.java
@@ -6,8 +6,8 @@ public class Q2 {
     @Test
     public void test() {
         System.out.println("deneme");
+        System.out.println("sena");
         System.out.println("deneme");
-        System.out.println("deneme");
-        System.out.println("deneme");
+        System.out.println("sena");
     }
 }
