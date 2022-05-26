f0b23b179966ebe54c0f900d8360f2612380938c
Senatrcn <80004386+Senatrcn@users.noreply.github.com>
2022-05-26 12:47:04 +0300

    sena commit3 (#3)
    Co-authored-by: Sena TERCAN <avsenatercan@gmail.com>

--- src/test/java/Alerts/Q2.java
+++ src/test/java/Alerts/Q2.java
@@ -6,10 +6,9 @@ import org.junit.Test;
 public class Q2 {
     @Test
     public void test() {
-        System.out.println("deneme");
-        System.out.println("deneme");
-        System.out.println("deneme");
-        System.out.println("deneme");
- 
+        System.out.println("sena");
+        System.out.println("sena");
+        System.out.println("sena");
+        System.out.println("sena");
     }
 }
